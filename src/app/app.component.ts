a9cf5815f04b0d4d52a18bb11e56d9cf127157b0
Abdessalem-ghodbeni <slouma4ghodbeny@gmail.com>
2023-10-17 12:06:33 +0100

    service and db.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first_work_shop';
+  x=10;
+  path='https://www.automobile-magazine.fr/asset/cms/840x394/174884/config/123638/la-future-bmw-serie-5-devrait-garder-une-calandre-au-format-encore-raisonnable.jpg?webp=1'
 }
