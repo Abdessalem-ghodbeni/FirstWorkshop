7a0e7792de5d50672a6955572543d76e5aae1918
Abdessalem-ghodbeni <slouma4ghodbeny@gmail.com>
2023-10-26 14:44:15 +0100

    crud using json_server

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import { User } from '../Modeles/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  // getUserById(id: number) {
-  //   let index = this.ListUser.findIndex((user) => user.idCustomer == id);
-  //   return this.ListUser[index];
-  // }
   getAllUser() {
     return this.http.get<User[]>('http://localhost:3000/users');
+  }
+  addUser(u: User) {
+    return this.http.post<User>(
+      ' http://localhost:3000/users',
+      u,
+      this.httpOptions
+    );
   }
   // addUser(u: User) {
   //   this.ListUser.push(u);
--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Modeles/user';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+  styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent {
-
+  myUser: User = {
+    id: 8,
+    firstName: 'holla',
+    lastName: 'Clooney',
+    birthDate: '1999-06-30',
+    accountCategory: 'Customer',
+    email: 'hollsa@brando.com',
+    password: 'test',
+    picture: 'https://bootdey.com/img/Content/avatar/avatar2.png',
+    profession: 'Software Engineer',
+  };
+  constructor(private _userService: UserService, private R: Router) {}
+  add() {
+    this._userService.addUser(this.myUser).subscribe(() => {
+      alert('added user');
+      this.R.navigate(['/user/user']);
+    });
+  }
 }
